f2fbda692ac761dec26880d19f8c3252eb8a716b
Paweł Zatoka <herganus@gmail.com>
2025-01-29 23:11:29 +0100

    Start readding dashboard panels

---
+++ src/browser/dashboard/layout-override.tsx
@@ -0,0 +1,47 @@
+import type { GameLayouts } from '@gtam-layouts/types'
+import type { ChangeEvent } from 'react'
+import { FormControl, FormControlLabel, FormLabel, Radio, RadioGroup } from '@mui/material'
+import { useReplicant } from '@nodecg/react-hooks'
+import { useMemo } from 'react'
+import { render } from '../render'
+import { DashboardThemeProvider } from './components/DashboardThemeProvider'
+
+export function LayoutOverride() {
+  const [gameLayouts, setGameLayouts] = useReplicant<GameLayouts>('gameLayouts', { bundle: 'gtam-layouts' })
+
+  const layoutOptions = useMemo(() => {
+    if (gameLayouts && gameLayouts.available) {
+      return gameLayouts.available.map((option) => {
+        return {
+          label: option.name,
+          value: option.code,
+        }
+      })
+    }
+    else {
+      return []
+    }
+  }, [gameLayouts])
+
+  const handleRadioChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const newValue = (event.target as HTMLInputElement).value
+    if (gameLayouts) {
+      setGameLayouts({ ...gameLayouts, selected: newValue })
+    }
+  }
+
+  return (
+    <DashboardThemeProvider>
+      <FormControl>
+        <FormLabel>Select layout override</FormLabel>
+        <RadioGroup value={gameLayouts?.selected} onChange={handleRadioChange}>
+          {layoutOptions.map(option => (
+            <FormControlLabel key={option.value} value={option.value} control={<Radio />} label={option.label} />
+          ))}
+        </RadioGroup>
+      </FormControl>
+    </DashboardThemeProvider>
+  )
+}
+
+render(<LayoutOverride />)
--- src/browser/hooks/useCurrentRun.ts
+++ src/browser/hooks/useCurrentRun.ts
@@ -3,17 +3,17 @@ import type { RunDataActiveRunSurrounding } from 'speedcontrol/types/schemas'
 import { useReplicant } from '@nodecg/react-hooks'
 
 function useCurrentRun() {
-  const [runDataActiveRunSurroundingRep] = useReplicant<RunDataActiveRunSurrounding>(
-    'runDataActiveRunSurrounding',
-    {
+  const [runDataActiveRunSurroundingRep]
+    = useReplicant<RunDataActiveRunSurrounding>('runDataActiveRunSurrounding', {
       bundle: 'nodecg-speedcontrol',
-    },
-  )
+    })
   const [runDataArrayRep] = useReplicant<RunDataArray>('runDataArray', {
     bundle: 'nodecg-speedcontrol',
   })
 
-  return (runDataArrayRep ?? []).find(run => run.id === runDataActiveRunSurroundingRep?.current)
+  return (runDataArrayRep ?? []).find(
+    run => run.id === runDataActiveRunSurroundingRep?.current,
+  )
 }
 
 export default useCurrentRun
