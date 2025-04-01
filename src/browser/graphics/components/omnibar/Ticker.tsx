ef5f92928b9e79fb6e22f0785a3a995879ef2276
Stormra1d <lennschlott@gmail.com>
2025-04-01 19:42:55 +0200

    fix: resolve comments

--- src/browser/graphics/components/omnibar/Ticker.tsx
+++ src/browser/graphics/components/omnibar/Ticker.tsx
@@ -7,7 +7,6 @@ import { NextRun } from './Ticker/NextRun'
 export function Ticker({ style }: { style: CSSProperties }) {
   const currentComponentIndex = useRef(0)
   const currentComponentContainer = useRef<HTMLSpanElement | null>(null)
-  const [timestamp, setTimestamp] = useState(Date.now())
   const tickerContainerRef = useRef<HTMLDivElement | null>(null)
   const [needsScrolling, setNeedsScrolling] = useState(false)
 
@@ -15,7 +14,7 @@ export function Ticker({ style }: { style: CSSProperties }) {
     genericMessage('Welcome to <span class="highlight">GTAMarathon 2025</span>! Enjoy the show!'),
     genericMessage('Check out the merch store over at <span class="highlight">merch.gtamarathon.com</span>!'),
     genericMessage(`Type <span class="highlight">!schedule</span> in the chat to see what's on next!`),
-    <NextRun key={timestamp} time={20} onEnd={showNextElement} containerRef={tickerContainerRef} onScrollingNeeded={setNeedsScrolling} />,
+    <NextRun key={currentComponentIndex.current} time={20} onEnd={showNextElement} containerRef={tickerContainerRef} onScrollingNeeded={setNeedsScrolling} />,
   ]
 
   const [currentElement, setCurrentElement] = useState<React.JSX.Element | undefined>(messageTypes[0])
@@ -26,7 +25,6 @@ export function Ticker({ style }: { style: CSSProperties }) {
     currentComponentIndex.current = (currentComponentIndex.current + 1) % messageTypes.length
     const nextElement = messageTypes[currentComponentIndex.current]
 
-    setTimestamp(Date.now())
     setCurrentElement(nextElement)
   }
 
